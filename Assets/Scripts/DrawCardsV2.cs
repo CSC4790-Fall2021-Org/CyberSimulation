ac8ea146764607617edb9c12156b442874603906
WilliamWantuch <55930416+WilliamWantuch@users.noreply.github.com>
2021-10-30 16:24:09 -0400

    Destroy Cards Button and Script
    release3

--- Assets/Scripts/DrawCardsV2.cs
+++ Assets/Scripts/DrawCardsV2.cs
@@ -60,7 +60,30 @@ public class DrawCardsV2 : MonoBehaviour
                 playerCard.transform.SetParent(PlayerArea.transform, false);
 
                 cards.Remove(tempCard);
+                //can store this tempCard in an array to add it back into the pool of selectable cards at a later time
             }
+        }
+    }
+
+    public void DestroyCards()
+    {
+        if(currentCards == 5)
+        {
+            Debug.Log("within onclick of destroy cards");
+            GameObject originalGameObject = GameObject.Find("PlayerArea");
+            GameObject child1 = originalGameObject.transform.GetChild(0).gameObject;
+            GameObject child2 = originalGameObject.transform.GetChild(1).gameObject;
+            GameObject child3 = originalGameObject.transform.GetChild(2).gameObject;
+            GameObject child4 = originalGameObject.transform.GetChild(3).gameObject;
+            GameObject child5 = originalGameObject.transform.GetChild(4).gameObject;
+
+            Destroy(child1);
+            Destroy(child2);
+            Destroy(child3);
+            Destroy(child4);
+            Destroy(child5);
+
+            currentCards -= 5;
         }
     }
 }
