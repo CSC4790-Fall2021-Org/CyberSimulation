51aa6c767e5e786aa4c8735eb486c7d6f8db7b2c
WilliamWantuch <55930416+WilliamWantuch@users.noreply.github.com>
2021-11-14 18:09:04 -0500

    Updated Card Popup Menu

--- Assets/Scripts/FlipCard.cs
+++ Assets/Scripts/FlipCard.cs
@@ -31,6 +31,8 @@ public class FlipCard : MonoBehaviour
     public GameObject Name;
     public GameObject Imagee;
     public GameObject drawCardsButton;
+    public GameObject windowMoney;
+    public string windowMoneyText;
 
     // Start is called before the first frame update
     void Start()
@@ -41,6 +43,8 @@ public class FlipCard : MonoBehaviour
         CardDescription = CardDescriptionParent.transform.Find("Card Window").gameObject;
         Description = CardDescription.transform.Find("desc").gameObject;
         Name = CardDescription.transform.Find("name").gameObject;
+        windowMoney = CardDescription.transform.Find("WindowMoney").gameObject;
+        windowMoneyText = windowMoney.GetComponent<Text>().text;
         description = Description.GetComponent<Text>();
         Imagee = CardDescription.transform.Find("CardImage").gameObject;
         image = Imagee.GetComponent<Image>();
@@ -82,99 +86,123 @@ public class FlipCard : MonoBehaviour
             //Debug.Log(mousePos.ToString());
         }
 
-        if(Input.GetMouseButtonDown(1))
+        if(drawCardsButton.GetComponent<DrawCardsV2>().lastCardDrawn == true && drawCardsButton.GetComponent<DrawCardsV2>().inChatManager == false)
         {
-            //StartFlip();
-            //  string temp = GameObject.Find("Ex").GetComponent<CSVScriptCard>().cardDescription[];
-            //GameObject.Find("Draw Cards Button").GetComponent<DrawCardsV2>().targetRect1;
-            //Alan's Code Here
-            /* 
-            ddrawc = GameObject.Find("Ex").GetComponent<CSVScriptCard>().cardDescription;
-            string tempp =  GameObject.Find("System").GetComponent<CSVScript>().endRoundSum[1];
-            description.text = tempp;
-            temp = Instantiate(canvas, new Vector3(0, 0, 0), Quaternion.identity);
-            popup(temp);
-            Debug.Log("in flip + temp " + tempp);
-            */
 
-            GameObject originalGameObject = GameObject.Find("PlayerArea");
-            GameObject child1 = drawCardsButton.GetComponent<DrawCardsV2>().child1;
-            GameObject child2 = drawCardsButton.GetComponent<DrawCardsV2>().child2;
-            GameObject child3 = drawCardsButton.GetComponent<DrawCardsV2>().child3;
-            GameObject child4 = drawCardsButton.GetComponent<DrawCardsV2>().child4;
-            GameObject child5 = drawCardsButton.GetComponent<DrawCardsV2>().child5;
-
-            RectTransform targetRect1 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect1;
-            RectTransform targetRect2 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect2;
-            RectTransform targetRect3 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect3;
-            RectTransform targetRect4 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect4;
-            RectTransform targetRect5 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect5;
-            
-            if (RectTransformUtility.RectangleContainsScreenPoint(targetRect1, mousePos))
+            if(Input.GetMouseButtonDown(1))
             {
-                string temp = "";
-             
-                nameCard = child1.GetComponent<CardDisplay>().nameText.text;
-                Debug.Log("namecard" + nameCard);
+                //StartFlip();
+                //  string temp = GameObject.Find("Ex").GetComponent<CSVScriptCard>().cardDescription[];
+                //GameObject.Find("Draw Cards Button").GetComponent<DrawCardsV2>().targetRect1;
+                //Alan's Code Here
+                /* 
+                ddrawc = GameObject.Find("Ex").GetComponent<CSVScriptCard>().cardDescription;
+                string tempp =  GameObject.Find("System").GetComponent<CSVScript>().endRoundSum[1];
+                description.text = tempp;
+                temp = Instantiate(canvas, new Vector3(0, 0, 0), Quaternion.identity);
+                popup(temp);
+                Debug.Log("in flip + temp " + tempp);
+                */
 
-                int d = ID.IndexOf(nameCard);
-              //  image = child1.GetComponent<Image>();
-                Debug.Log("d" + d);
-               description.text = ddrawc[d];
-                name.text = ID[d];
-                CardDescription.SetActive(true);
-                drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
-            }
+                GameObject originalGameObject = GameObject.Find("PlayerArea");
+                GameObject child1 = drawCardsButton.GetComponent<DrawCardsV2>().child1;
+                GameObject child2 = drawCardsButton.GetComponent<DrawCardsV2>().child2;
+                GameObject child3 = drawCardsButton.GetComponent<DrawCardsV2>().child3;
+                GameObject child4 = drawCardsButton.GetComponent<DrawCardsV2>().child4;
+                GameObject child5 = drawCardsButton.GetComponent<DrawCardsV2>().child5;
 
-            if (RectTransformUtility.RectangleContainsScreenPoint(targetRect2, mousePos))
-            {
-                nameCard = child2.GetComponent<CardDisplay>().nameText.text;
-                Debug.Log("namecard" + nameCard);
-                int d = ID.IndexOf(nameCard);
-                Debug.Log("d" + d);
-                name.text = ID[d];
+                RectTransform targetRect1 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect1;
+                RectTransform targetRect2 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect2;
+                RectTransform targetRect3 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect3;
+                RectTransform targetRect4 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect4;
+                RectTransform targetRect5 = drawCardsButton.GetComponent<DrawCardsV2>().targetRect5;
+                
+                if (RectTransformUtility.RectangleContainsScreenPoint(targetRect1, mousePos))
+                {
+                    CardDescription.transform.Find("WindowMoney").GetComponent<Text>().text = child1.GetComponent<CardDisplay>().moneyValue.text;
+
+                    CardDescription.transform.Find("CardImage").GetComponent<Image>().sprite = child1.transform.GetChild(0).GetComponent<Image>().sprite;
+
+                    string temp = "";
+                
+                    nameCard = child1.GetComponent<CardDisplay>().nameText.text;
+                    Debug.Log("namecard" + nameCard);
+
+                    int d = ID.IndexOf(nameCard);
+                //  image = child1.GetComponent<Image>();
+                    Debug.Log("d" + d);
                 description.text = ddrawc[d];
-                CardDescription.SetActive(true);
-                drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
+                    name.text = ID[d];
+                    CardDescription.SetActive(true);
+                    drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
+                }
+
+                if (RectTransformUtility.RectangleContainsScreenPoint(targetRect2, mousePos))
+                {
+                    CardDescription.transform.Find("WindowMoney").GetComponent<Text>().text = child2.GetComponent<CardDisplay>().moneyValue.text;
+
+                    CardDescription.transform.Find("CardImage").GetComponent<Image>().sprite = child2.transform.GetChild(0).GetComponent<Image>().sprite;
+
+                    nameCard = child2.GetComponent<CardDisplay>().nameText.text;
+                    Debug.Log("namecard" + nameCard);
+                    int d = ID.IndexOf(nameCard);
+                    Debug.Log("d" + d);
+                    name.text = ID[d];
+                    description.text = ddrawc[d];
+                    CardDescription.SetActive(true);
+                    drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
+                }
+
+                if (RectTransformUtility.RectangleContainsScreenPoint(targetRect3, mousePos))
+                {
+                    CardDescription.transform.Find("WindowMoney").GetComponent<Text>().text = child3.GetComponent<CardDisplay>().moneyValue.text;
+
+                    CardDescription.transform.Find("CardImage").GetComponent<Image>().sprite = child3.transform.GetChild(0).GetComponent<Image>().sprite;
+
+                    nameCard = child3.GetComponent<CardDisplay>().nameText.text;
+                    Debug.Log("namecard" + nameCard);
+                    int d = ID.IndexOf(nameCard);
+                    Debug.Log("d" + d);
+                    name.text = ID[d];
+                    description.text = ddrawc[d];
+                    CardDescription.SetActive(true);
+                    drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
+                }
+
+                if (RectTransformUtility.RectangleContainsScreenPoint(targetRect4, mousePos))
+                {
+                    CardDescription.transform.Find("WindowMoney").GetComponent<Text>().text = child4.GetComponent<CardDisplay>().moneyValue.text;
+
+                    CardDescription.transform.Find("CardImage").GetComponent<Image>().sprite = child4.transform.GetChild(0).GetComponent<Image>().sprite;
+
+                    nameCard = child4.GetComponent<CardDisplay>().nameText.text;
+                    Debug.Log("namecard" + nameCard);
+                    int d = ID.IndexOf(nameCard);
+                    Debug.Log("d" + d);
+                    name.text = ID[d];
+                    description.text = ddrawc[d];
+                    CardDescription.SetActive(true);
+                    drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
+                }
+
+                if (RectTransformUtility.RectangleContainsScreenPoint(targetRect5, mousePos))
+                {
+                    CardDescription.transform.Find("WindowMoney").GetComponent<Text>().text = child5.GetComponent<CardDisplay>().moneyValue.text;
+
+                    CardDescription.transform.Find("CardImage").GetComponent<Image>().sprite = child5.transform.GetChild(0).GetComponent<Image>().sprite;
+
+                    nameCard = child5.GetComponent<CardDisplay>().nameText.text;
+                    Debug.Log("namecard" + nameCard);
+                    int d = ID.IndexOf(nameCard);
+                    Debug.Log("d" + d);
+                    name.text = ID[d];
+                    description.text = ddrawc[d];
+                    CardDescription.SetActive(true);
+                    drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
+                }
+
+
             }
-
-            if (RectTransformUtility.RectangleContainsScreenPoint(targetRect3, mousePos))
-            {
-                nameCard = child3.GetComponent<CardDisplay>().nameText.text;
-                Debug.Log("namecard" + nameCard);
-                int d = ID.IndexOf(nameCard);
-                Debug.Log("d" + d);
-                name.text = ID[d];
-                description.text = ddrawc[d];
-                CardDescription.SetActive(true);
-                drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
-            }
-
-            if (RectTransformUtility.RectangleContainsScreenPoint(targetRect4, mousePos))
-            {
-                nameCard = child4.GetComponent<CardDisplay>().nameText.text;
-                Debug.Log("namecard" + nameCard);
-                int d = ID.IndexOf(nameCard);
-                Debug.Log("d" + d);
-                name.text = ID[d];
-                description.text = ddrawc[d];
-                CardDescription.SetActive(true);
-                drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
-            }
-
-            if (RectTransformUtility.RectangleContainsScreenPoint(targetRect5, mousePos))
-            {
-                nameCard = child5.GetComponent<CardDisplay>().nameText.text;
-                Debug.Log("namecard" + nameCard);
-                int d = ID.IndexOf(nameCard);
-                Debug.Log("d" + d);
-                name.text = ID[d];
-                description.text = ddrawc[d];
-                CardDescription.SetActive(true);
-                drawCardsButton.GetComponent<DrawCardsV2>().inChatManager = true;
-            }
-
-
         }
     }
     public void closee(Canvas canvas)
